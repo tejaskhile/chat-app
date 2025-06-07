6d4a779007065e999b1947d9a32383830c42923c
tejaskhile <tejaskhile07@gmail.com>
2025-06-07 17:37:43 +0530

    rendering issue fixed

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-    }
-})
+  baseURL: process.env.REACT_APP_API_URL || process.env.RENDER_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
 
-export default axiosInstance
+export default axiosInstance;
